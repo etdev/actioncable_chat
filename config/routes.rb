bff3f2e3075a40f7c0f823b9e0e6b145db54439c
Eric Turner <ericturnerdev@gmail.com>
2015-09-07 22:58:12 +0900

    Add messages page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :events, only: [:new, :create, :show]
+  resources :messages, only: [:index]
   # custom bootstrap styles test page
   get "bs_test" => "tests#show"
   get "about" => "static#about", as: :about
